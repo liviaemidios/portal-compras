ead77b228f10a4ef393d58cd2e595d0fd0a93fdf
liviaemidios <110050714+liviaemidios@users.noreply.github.com>
2025-05-05 01:08:08 -0300

    Update dados_produtos.py

--- dados_produtos.py
+++ dados_produtos.py
@@ -4,16 +4,14 @@ import os
 
 CAMINHO_PRODUTOS = "database/produtos.csv"
 
-def inicializar_csv_produtos():
-    if not os.path.exists(CAMINHO_PRODUTOS):
-        colunas = ["nome", "categoria", "unidade", "preco", "descricao"]
-        pd.DataFrame(columns=colunas).to_csv(CAMINHO_PRODUTOS, index=False)
+COLUNAS_PRODUTOS = [
+    "produto", "marca", "unidade", "preco", "fornecedor"
+]
 
 def carregar_produtos():
-    if os.path.exists(CAMINHO_PRODUTOS):
-        return pd.read_csv(CAMINHO_PRODUTOS)
-    else:
-        return pd.DataFrame(columns=["nome", "categoria", "unidade", "preco", "descricao"])
+    if not os.path.exists(CAMINHO_PRODUTOS):
+        return pd.DataFrame(columns=COLUNAS_PRODUTOS)
+    return pd.read_csv(CAMINHO_PRODUTOS, dtype=str)
 
 def salvar_produtos(df):
     df.to_csv(CAMINHO_PRODUTOS, index=False)
