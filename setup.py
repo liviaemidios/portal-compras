114b7ad35203a0c55a03b71e1324e206ed73f65f
liviaemidios <110050714+liviaemidios@users.noreply.github.com>
2025-05-04 23:27:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from dados_fornecedores import inicializar_csv_fornecedores
 from dados_produtos import inicializar_csv_produtos
-from dados_distribuidoras import inicializar_csv_distribuidoras
+from dados_concorrentes import inicializar_csv_concorrentes
 
 
 def inicializar_sistema():
     print("🔧 Inicializando estrutura de dados...")
     inicializar_csv_fornecedores()
     inicializar_csv_produtos()
-    inicializar_csv_distribuidoras()
+    inicializar_csv_concorrentes()
     print("✅ Todos os arquivos foram criados ou já existiam.")
 
 
