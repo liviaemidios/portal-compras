2d0adc7541ca8c2d46005888c5c4223cfeb7b08d
liviaemidios <110050714+liviaemidios@users.noreply.github.com>
2025-05-04 23:38:00 -0300

    Update dados_concorrentes.py

--- dados_concorrentes.py
+++ dados_concorrentes.py
@@ -1,13 +1,25 @@
+# dados_concorrentes.py
 import pandas as pd
 import os
 
 CAMINHO_CONCORRENTES = "database/concorrentes.csv"
 
+def inicializar_csv_concorrentes():
+    if not os.path.exists(CAMINHO_CONCORRENTES):
+        colunas = [
+            "razao_social", "nome_fantasia", "cnpj", "telefone", "email", "endereco",
+            "inscricao_estadual", "inscricao_municipal", "pedido_minimo", "prazo_pagamento"
+        ]
+        pd.DataFrame(columns=colunas).to_csv(CAMINHO_CONCORRENTES, index=False)
+
 def carregar_concorrentes():
     if os.path.exists(CAMINHO_CONCORRENTES):
         return pd.read_csv(CAMINHO_CONCORRENTES)
     else:
-        return pd.DataFrame(columns=["razao_social", "nome_fantasia", "cnpj", "email", "telefone"])
+        return pd.DataFrame(columns=[
+            "razao_social", "nome_fantasia", "cnpj", "telefone", "email", "endereco",
+            "inscricao_estadual", "inscricao_municipal", "pedido_minimo", "prazo_pagamento"
+        ])
 
 def salvar_concorrentes(df):
     df.to_csv(CAMINHO_CONCORRENTES, index=False)
